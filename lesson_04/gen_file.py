6d6f6ae752898b04c4ef4203ede48bee9e8a27d7
Alex <alex.ba.ua@gmail.com>
2022-11-03 18:57:45 +0200

    next try

--- lesson_04/gen_file.py
+++ lesson_04/gen_file.py
@@ -3,17 +3,18 @@ from typing import Generator
 import os
 
 LESSON_4_DIR = Path(__file__).parent
-TEMPFILE = LESSON_4_DIR / "temprock.txt"
-ROCKYOU_FILENAME = LESSON_4_DIR / "rockyou.txt"
+TEMPROCK = LESSON_4_DIR / "temprock.txt"
+ROCKYOU = LESSON_4_DIR / "rockyou.txt"
 
 input_date = input()
 
-j = []
+coinc_list = []
+
 
 def filter_lines(filename: Path, pattern: str) -> Generator:
-    with open(filename, encoding="utf-8") as file:
+    with open(filename, encoding="utf-8") as f:
         while True:
-            line = file.readline().replace("\n", "")
+            line = f.readline().replace("\n", "")
 
             if not line:
                 break
@@ -22,17 +23,15 @@ def filter_lines(filename: Path, pattern: str) -> Generator:
                 yield line
 
 
-for d in filter_lines(ROCKYOU_FILENAME, input_date):
-    j.append(d + "\n")
+for d in filter_lines(ROCKYOU, input_date):
+    coinc_list.append(d + "\n")
 
-with open(TEMPFILE, "w") as file:
-    file.writelines(j)
+with open(TEMPROCK, "w") as file:
+    file.writelines(coinc_list)
 
-
-stats = os.stat(TEMPFILE)
+stats = os.stat(TEMPROCK)
 print(stats.st_size, "bites")
 
-with open(TEMPFILE) as file:
+with open(TEMPROCK) as file:
     size = len([0 for _ in file])
 print(size, "lines")
-
